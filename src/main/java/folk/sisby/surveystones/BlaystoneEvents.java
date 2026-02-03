5bf37fb3bb746788f7ee71ccecf54ec177dc16cd
sisby-folk <sleepingdragoninn@gmail.com>
2026-02-03 15:48:11 +1100

    work around blockstate unavailability when removing waystone

--- src/main/java/folk/sisby/surveystones/BlaystoneEvents.java
+++ src/main/java/folk/sisby/surveystones/BlaystoneEvents.java
@@ -56,7 +56,7 @@ public class BlaystoneEvents {
 	public static void removeWaystone(World world, IWaystone waystone) {
 		WorldLandmarks landmarks = WorldLandmarks.of(world);
 		if (landmarks == null) return;
-		landmarks.remove(WorldLandmarks.GLOBAL, getId(world, waystone));
+		landmarks.removeAll(l -> l.id().toString().startsWith("waystones:" + waystone.getWaystoneType().getPath()) && l.contains(LandmarkComponentTypes.POS) && l.get(LandmarkComponentTypes.POS).equals(waystone.getPos()) || l.get(LandmarkComponentTypes.POS).equals(waystone.getPos().down()));
 	}
 
 	public static void activateWaystone(WaystoneActivatedEvent event) {
